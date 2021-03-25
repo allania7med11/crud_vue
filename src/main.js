71b37b7ef3dc78adcddd5e7d37eade779dd9f7de
Allani Ahmed <67459843+ahmedallani@users.noreply.github.com>
2021-03-25 05:25:09 +0100

    tailwind

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
+import './assets/tailwind.css'
 
 Vue.use(VueAxios, axios);
 Vue.config.productionTip = false;
